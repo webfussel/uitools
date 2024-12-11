2a03c89f8fb76777071ccc6c797c08c4b18d276c
webfussel <fiona@webfussel.de>
2024-12-11 12:33:55 +0100

    FIX: icon bundle
    fix icon bundles for SSG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,5 +39,14 @@ export default defineNuxtConfig({
   shiki: {
     defaultTheme: 'catppuccin-frappe',
     bundledLangs: ['css']
-  }
+  },
+  icon: {
+    clientBundle: {
+      scan: {
+        // note that when you specify those values, the default behavior will be overridden
+        globInclude: ['components/**/*.vue', /* ... */],
+        globExclude: ['node_modules', 'dist', /* ... */],
+      },
+    },
+  },
 })
